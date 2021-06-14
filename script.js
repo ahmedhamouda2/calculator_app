5009e30f05a985576941448925823b7cf399951d
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-06-14 15:32:09 +0300

    create function of switch between themes

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ back = () => {
     let exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0,exp.length-1)
 }
+
+// create function of switch between themes when click of button
+let btnChange = document.getElementById("btn-switch")
+btnChange.addEventListener("click" , toggleTheme )
+function toggleTheme() {
+    let theme = document.getElementsByTagName('link')[5];
+
+    if (theme.getAttribute('href') == 'default.css') {
+        theme.setAttribute('href', 'theme1.css');
+    } else if (theme.getAttribute('href') == 'theme1.css') {
+        theme.setAttribute('href', 'theme2.css');
+    } else {
+        theme.setAttribute('href', 'default.css');
+    }
+}
